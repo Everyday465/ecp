040156072ce572180f201f0f13137395f229c330
Everyday465 <110691604+Everyday465@users.noreply.github.com>
2024-11-26 19:34:39 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,8 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_581XRbCkc', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6uqgsv8j7rgnj1khep12b4v6gg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2,
+        clientSecret: '9r8olrqf3s8a1q380pbpd30kj4q311tta0e7c87sd4ilfcc7lkb'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
